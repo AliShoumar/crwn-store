141ad8f90ec98565d6ae6e39b3e40baee10b13ba
ShoumarAli <alialakbarshoumar610@gmail.com>
2023-08-25 01:47:52 +0300

    update our menu item to navigate using useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 import HomePage from './page/homepage.component';
+
+const HatPage = () => {
+	return 'This is hat page';
+};
 
 function App() {
 	return (
 		<div className='App'>
-			<HomePage />
+			<Routes>
+				<Route path='/'>
+					<Route index element={<HomePage />} />
+					<Route path='/hats' element={<HatPage />}></Route>
+				</Route>
+			</Routes>
 		</div>
 	);
 }
